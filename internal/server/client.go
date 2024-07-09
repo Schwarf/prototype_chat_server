5ba6d0c8b46adbae6669107ae40ce84437216a2d
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-07-09 16:37:19 +0200

    Work on client server structure.

--- internal/server/client.go
+++ internal/server/client.go
@@ -1,1 +1,11 @@
 package server
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	ID         string
+	Connection *websocket.Conn
+	Server     *Server
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,17 @@
 package server
+
+import (
+	"github.com/Schwarf/prototype_chat_server/internal/models"
+	"github.com/Schwarf/prototype_chat_server/pkg/config"
+	"github.com/gorilla/websocket"
+	"sync"
+)
+
+type Server struct {
+	config    *config.Config
+	clients   map[*Client]bool
+	broadcast chan models.Message
+	mu        sync.Mutex
+	db        *storage.DB
+	upgrader  websocket.Upgrader
+}
