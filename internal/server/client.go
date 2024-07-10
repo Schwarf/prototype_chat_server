387b7d57cdd9c94c2a5aa49f2da8c06895b0c470
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-07-10 17:52:32 +0200

    Add client SendMessage

--- internal/server/client.go
+++ internal/server/client.go
@@ -8,4 +8,9 @@ type Client struct {
 	ID         string
 	Connection *websocket.Conn
 	Server     *Server
+	Online     bool
+}
+
+func (c *Client) SendMessage(messageType int, message []byte) error {
+	return c.Connection.WriteMessage(messageType, message)
 }
